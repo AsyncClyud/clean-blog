6612956fd943209d0737fee7764f8b1400e87b45
AsyncClyud <AsyncClyud@outlook.com>
2026-07-14 13:29:33 -0400

    Add security Headers for all roads

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -7,16 +7,31 @@ import (
 	"net/http"
 )
 
-type AuthMiddleware struct {
+type Middleware struct {
 	Middleware service.AuthService
 }
 
-func NewAuthMiddleware(service service.AuthService) *AuthMiddleware {
-	return &AuthMiddleware{Middleware: service}
+func NewAuthMiddleware(service service.AuthService) *Middleware {
+	return &Middleware{Middleware: service}
 }
 
-func (md AuthMiddleware) RequireAuth(next http.Handler) http.Handler {
+func (md Middleware) SecureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+		w.Header().Set("Permissions-Policy", "geolocation=(), microphone=(), camera=()")
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (md Middleware) RequireAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+		w.Header().Set("Permissions-Policy", "geolocation=(), microphone=(), camera=()")
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
 
 		cookie, err := r.Cookie("jwt-token")
 		if err != nil {
