06ccfe1e26fb53d876f9ef0cf4a2d226308e07cd
AsyncClyud <AsyncClyud@outlook.com>
2026-07-14 13:38:25 -0400

    Add settings, fix URLs and minor design changes

--- web/profile/scripts/profile.js
+++ web/profile/scripts/profile.js
@@ -7,14 +7,15 @@ async function FetchUserProfile() {
   });
   if (response.ok) {
     const data = JSON.parse(await response.json())
-    var main_element = document.getElementById("main")
-    var user_info = document.createElement("div")
+    const main_element = document.getElementById("main")
+    const user_info = document.createElement("div")
     user_info.setAttribute("id", "profile")
     user_info.innerHTML = `
       <img src="profile/images/avatar.jpg" alt="Avatar" width="200" height="200">
       <h3>${data.Username}</h3>
       <p>${data.Bio}</p>
       <p>${data.Created_at}</p>
+      <a href="profile/settings">Settings</a>
     `
     main_element.appendChild(user_info)
     document.title = `${data.Username} profile`
@@ -23,3 +24,5 @@ async function FetchUserProfile() {
     window.location.replace("/auth/register")
   }
 }
+
+export default FetchUserProfile()
---
+++ web/profile/scripts/settings.js
@@ -0,0 +1,63 @@
+"use strict";
+
+async function FetchUserInfo() {
+  const response = await fetch("/api/profile", {
+    method: "GET",
+    headers: { Accept: "application/json" },
+  })
+  if (response.ok) {
+    const data = JSON.parse(await response.json())
+    document.getElementById("username").textContent = `${data.Username}`
+    document.getElementById("bio").textContent = `${data.Bio}`
+  }
+}
+
+async function SendUpdateUsernameRequest() {
+  const new_username = document.getElementById("input_username").value
+  const response = await fetch("/api/profile/username", {
+    method: "PUT",
+    headers: { Accept: "application/json" },
+    body: JSON.stringify({
+      Username: new_username
+    })
+
+  })
+  if (response.ok) {
+    const data = JSON.parse(await response.json())
+    document.getElementById("message_username").textContent = data
+  }
+}
+
+async function SendUpdateBioRequest() {
+  const new_bio = document.getElementById("input_bio").value
+  const response = await fetch("/api/profile/bio", {
+    method: "PUT",
+    headers: { Accept: "application/json" },
+    body: JSON.stringify({
+      Bio: new_bio
+    })
+
+  })
+  if (response.ok) {
+    const data = JSON.parse(await response.json())
+    document.getElementById("message_bio").textContent = data
+  }
+}
+
+async function SendUpdatePasswordRequest() {
+  const old_password = document.getElementById("input_old_password").value
+  const new_password = document.getElementById("input_new_password").value
+  const response = await fetch("/api/profile/password", {
+    method: "PUT",
+    headers: { Accept: "application/json" },
+    body: JSON.stringify({
+      Old_Password: old_password,
+      New_Password: new_password
+    })
+
+  })
+  if (response.ok) {
+    const data = JSON.parse(await response.json())
+    document.getElementById("message_password").textContent = data
+  }
+}
